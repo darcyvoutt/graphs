68d4bbb163e32125ee900053e0dede2a15df5d72
Darcy Voutt <darcyvoutt@gmail.com>
2014-04-05 15:05:37 -0400

    removed barcharts.js, no longer needed

--- assets/js/barcharts.js
+++
@@ -1,7 +0,0 @@
-// Produces width of .barChart
-$(document).ready(function() {
-  $('.graph-bar').each(function() {
-     var dataWidth = $(this).data('value');
-     $(this).css("width", dataWidth + "%");
-  });
-});
