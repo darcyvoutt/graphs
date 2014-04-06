b5777ffefa7177711ee84937813bca7fb179d9be
Darcy Voutt <darcyvoutt@gmail.com>
2014-04-05 23:00:08 -0400

    cleaned the bubbleChart less code

--- bubblechart.php
+++ bubblechart.php
@@ -1,12 +1,16 @@
 <div class="graphWidget">
+  
   <h2>Bubble Chart</h2>
-  <div class="chart">    
+  
+  <div class="bubbleChart">
     
     <svg>
-      <circle cx="50%" cy="50%" r="70" />        
+      <circle cx="50%" cy="50%" r="70" />      
       <circle cx="10%" cy="60%" r="100" />    
     </svg>
 
   <?php include('graphlines.php'); ?>
+
   </div>  
+  
 </div>
