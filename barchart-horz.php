1b8edf99c3f73acbdd6ff22922ff99d31a9864aa
Darcy Voutt <darcyvoutt@gmail.com>
2014-04-05 22:26:33 -0400

    added variables to bargraph and updated legend for better responsiveness

--- barchart-horz.php
+++ barchart-horz.php
@@ -1,3 +1,20 @@
+<?php 
+// Legend Variables
+$legend1 = "Year 1";
+$legend2 = "Year 2";
+$legend3 = "Year 3";
+$legend4 = "Year 4";
+$legend5 = "Year 5";
+
+// Value Variables
+$value1 = "90%";
+$value2 = "67%";
+$value3 = "59%";
+$value4 = "45%";
+$value5 = "46%";
+
+?>
+
 <div class="graphWidget">
 
   <h2>Horzintal Bar Chart</h2>
@@ -5,34 +22,34 @@
   <div class="barGraph">
 
     <ul class="barGraph-legend">
-      <li class="barGraph-legend-item">Mon</li>
-      <li class="barGraph-legend-item">Tue</li>
-      <li class="barGraph-legend-item">Wed</li>
-      <li class="barGraph-legend-item">Thu</li>
-      <li class="barGraph-legend-item">Fri</li>
+      <li class="barGraph-legend-item"><?php echo $legend1 ?></li>
+      <li class="barGraph-legend-item"><?php echo $legend2 ?></li>
+      <li class="barGraph-legend-item"><?php echo $legend3 ?></li>
+      <li class="barGraph-legend-item"><?php echo $legend4 ?></li>
+      <li class="barGraph-legend-item"><?php echo $legend5 ?></li>
     </ul>
 
     <div class="barGraph-graph">
 
       <div class="barGraph-bar">
-        <svg><line x1="0%" y1="0%" x2="28.5%" y2="0%" /></svg>
-        <span class="barGraph-tooltip">80%</span>
+        <svg><line x1="0%" y1="0%" x2="<?php echo $value1 ?>" y2="0%" /></svg>
+        <span class="barGraph-tooltip"><?php echo $value1 ?></span>
       </div>
       <div class="barGraph-bar">
-        <svg><line x1="0%" y1="0%" x2="85%" y2="0%" /></svg>
-        <span class="barGraph-tooltip">40%</span>
+        <svg><line x1="0%" y1="0%" x2="<?php echo $value2 ?>" y2="0%" /></svg>
+        <span class="barGraph-tooltip"><?php echo $value2 ?></span>
       </div>
       <div class="barGraph-bar">
-        <svg><line x1="0%" y1="0%" x2="70%" y2="0%" /></svg>
-        <span class="barGraph-tooltip">40%</span>
+        <svg><line x1="0%" y1="0%" x2="<?php echo $value3 ?>" y2="0%" /></svg>
+        <span class="barGraph-tooltip"><?php echo $value3 ?></span>
       </div>
       <div class="barGraph-bar">
-        <svg><line x1="0%" y1="0%" x2="50%" y2="0%" /></svg>
-        <span class="barGraph-tooltip">40%</span>
+        <svg><line x1="0%" y1="0%" x2="<?php echo $value4 ?>" y2="0%" /></svg>
+        <span class="barGraph-tooltip"><?php echo $value4 ?></span>
       </div>
       <div class="barGraph-bar">
-        <svg><line x1="0%" y1="0%" x2="68%" y2="0%" /></svg>
-        <span class="barGraph-tooltip">40%</span>
+        <svg><line x1="0%" y1="0%" x2="<?php echo $value5 ?>" y2="0%" /></svg>
+        <span class="barGraph-tooltip"><?php echo $value5 ?></span>
       </div>
 
     </div>    
