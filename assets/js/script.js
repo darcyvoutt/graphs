28f839222a2770b8c9b225ca7438e0507adf548c
Darcy Voutt <darcyvoutt@gmail.com>
2014-07-04 07:04:49 -0400

    fixed the xAxis labels by changing datum

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,16 +9,12 @@ nv.addGraph(function() {
     .delay(350)
     ;
 
-  var tickMarks = ["YourPhone","Universe X3", "ePhone 74s", "NextUs", "Humanoid"];
-
-  chart.xAxis      
-      // .tickFormat(d3.format(',f'))
-      .tickValues(tickMarks)
+  chart.xAxis       
       .axisLabel("Products")
       ;
 
   chart.yAxis
-      .tickFormat(d3.format(',1f'))
+      .tickFormat(d3.format(',1r'))
       ;
 
   d3.select('#salesChart svg')
@@ -36,76 +32,36 @@ function exampleData() {
   return [
     {
       "key": "Basic Planners",
-      "values": [{
-          "x": 0,
-          "y": 150
-      }, {
-          "x": 1,
-          "y": 300
-      }, {
-          "x": 2,
-          "y": 1500
-      }, {
-          "x": 3,
-          "y": 50
-      }, {
-          "x": 4,
-          "y": 500
-      }]
+      "values": [{"x": "YourPhone","y": 150}, 
+                 {"x": "Universe X3","y": 300},
+                 {"x": "ePhone 74s","y": 1500}, 
+                 {"x": "NextUs","y": 50}, 
+                 {"x": "Humanoid","y": 500
+                }]
     }, {
       "key": "No-Namers",
-      "values": [{
-          "x": 0,
-          "y": 300
-      }, {
-          "x": 1,
-          "y": 250
-      }, {
-          "x": 2,
-          "y": 400
-      }, {
-          "x": 3,
-          "y": 150
-      }, {
-          "x": 4,
-          "y": 900
-      }]
+      "values": [{"x": "YourPhone","y": 300}, 
+                 {"x": "Universe X3","y": 250}, 
+                 {"x": "ePhone 74s","y": 400}, 
+                 {"x": "NextUs","y": 150}, 
+                 {"x": "Humanoid","y": 900}]
+
     }, {
       "key": "Feature Followers",
-      "values": [{
-          "x": 0,
-          "y": 350
-      }, {
-          "x": 1,
-          "y": 900
-      }, {
-          "x": 2,
-          "y": 100
-      }, {
-          "x": 3,
-          "y": 500
-      }, {
-          "x": 4,
-          "y": 250
-      }]
+      "values": [{"x": "YourPhone","y": 350}, 
+                 {"x": "Universe X3","y": 900}, 
+                 {"x": "ePhone 74s","y": 100}, 
+                 {"x": "NextUs","y": 500}, 
+                 {"x": "Humanoid","y": 250}]
+
     }, {
       "key": "Hipsters & Elites",
-      "values": [{
-          "x": 0,
-          "y": 200
-      }, {
-          "x": 1,
-          "y": 350
-      }, {
-          "x": 2,
-          "y": 50
-      }, {
-          "x": 3,
-          "y": 800
-      }, {
-          "x": 4,
-          "y": 100
-      }]
+      "values": [{"x": "YourPhone","y": 200}, 
+                 {"x": "Universe X3","y": 350}, 
+                 {"x": "ePhone 74s","y": 50}, 
+                 {"x": "NextUs","y": 800}, 
+                 {"x": "Humanoid","y": 100}]
+
     }
   ]
 }
